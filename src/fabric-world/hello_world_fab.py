31da94cacef57a660e9fda9a7741d047339bc596
Rajiv Sahu <rajivsahu10@gmail.com>
2019-03-28 04:15:03 +0530

    renamed the file

--- src/fabric-world/hello_world_fab.py
+++ src/fabric-world/hello_world_fab.py
@@ -3,7 +3,7 @@ import fabric
 
 def main():
     # machines = ['192.168.1.6']
-    con = fabric.Connection('192.168.1.6',user='raj', connect_kwargs={"password": "ae"})
+    con = fabric.Connection('192.168.1.6',user='raj', connect_kwargs={"password": "aaaaaaaa"})
     con.run('ls -l')
 
 
